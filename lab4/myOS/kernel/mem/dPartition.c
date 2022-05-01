d2cbbae690abffa4caaf0690dac253480c3e34b3
Bill <cgx@mail.ustc.edu.cn>
2022-05-01 16:51:34 +0800

    Finish dPartition.c.

--- lab4/myOS/kernel/mem/dPartition.c
+++ lab4/myOS/kernel/mem/dPartition.c
@@ -1,6 +1,5 @@
 #include "libio.h"
 
-
 //dPartition 是整个动态分区内存的数据结构
 typedef struct dPartition {
 	unsigned long size;
@@ -14,14 +13,15 @@ void show_dPartition(struct dPartition *dp) {
 // EMB每一个block的数据结构，userdata可以暂时不用管。
 typedef struct EMB {
 	unsigned long size;
-	union {
-		unsigned long nextStart;    // if free: pointer to next block
-        unsigned long userData;		// if allocated, belongs to user
-	};	                           
-} EMB;	// 共占8个字节
+    unsigned long free;
+    struct EMB* prev; // 表示上一个EMB的起始地址，不一定是空闲的，为了便于释放内存
+    struct EMB* nextStart;
+    struct EMB* prevStart; // 链表中上一个空闲块的地址
+} EMB;	// 共占20个字节
 
 void showEMB(struct EMB * emb) {
-	myPrintk(0x3,"EMB(start=0x%x, size=0x%x, nextStart=0x%x)\n", emb, emb->size, emb->nextStart);
+	myPrintk(0x3,"EMB(start=%p, free=%d, size=0x%x, nextStart=%p, prevStart=%p)\n",
+             emb, emb->free, emb->size, emb->nextStart, emb->prevStart);
 }
 
 unsigned long dPartitionInit(unsigned long start, unsigned long totalSize) {
@@ -38,7 +38,7 @@ unsigned long dPartitionInit(unsigned long start, unsigned long totalSize) {
     dPartition* handle = (dPartition*)start;
     EMB* emb = (EMB*)(start + sizeof(dPartition));
     *handle = (dPartition){.size = totalSize - sizeof(dPartition), .firstFreeStart = (unsigned long)emb};
-    *emb = (EMB){.size = handle->size - sizeof(EMB), .nextStart = 0};
+    *emb = (EMB){.size = handle->size - sizeof(EMB), .free = 1, .prev = 0, .nextStart = 0};
     return start;
 }
 
@@ -50,8 +50,9 @@ void dPartitionWalkByAddr(unsigned long dp) {
 	*/
     dPartition* d_par =(dPartition*)dp;
     show_dPartition(d_par);
-    for (EMB* emb = (EMB*)(d_par->firstFreeStart); emb; emb = (EMB*)emb->nextStart)
-        showEMB(emb);
+    for (unsigned long i = dp + sizeof(dPartition); i - dp - sizeof(dPartition) <= d_par->size;
+        i = ((EMB*)i)->size + i + sizeof(EMB))
+        showEMB((EMB*)i);
 }
 
 //=================firstfit, order: address, low-->high=====================
@@ -70,41 +71,68 @@ unsigned long dPartitionAllocFirstFit(unsigned long dp, unsigned long size){
             1.EMB类型的数据的存在本身就占用了一定的空间。
 	*/
     dPartition* d_par = (dPartition*)dp;
-    EMB* emb = (EMB*)(d_par->firstFreeStart), * emb_pre;
+    EMB* emb = (EMB*)(d_par->firstFreeStart);
     int flag = 0;
-    for (; emb; emb = (EMB*)(emb->nextStart)) {
-        if (emb->size >= size) { flag = 1; break; }
-        emb_pre = emb;
+    for (; emb; emb = emb->nextStart) {
+        if (emb->size >= size + sizeof(EMB)
+            | emb->size == size) { flag = 1; break; }
     }
     if (!flag) return 0;
     if (emb->size == size) {
-        if (emb == (EMB*)(d_par->firstFreeStart))
-            d_par->firstFreeStart = emb->nextStart;
-        else emb_pre->nextStart = emb->nextStart;
+        if (emb->prevStart == 0) // Head of the list.
+            d_par->firstFreeStart = (unsigned long)emb->nextStart;
+        else emb->prevStart->nextStart = emb->nextStart;
+        emb->free = 0;
     }
     else {
-        unsigned long new_emp_pos = (unsigned long)emb + size;
-        EMB tmp = (EMB){.size = emb->size - size, .nextStart = emb->nextStart};
-        *(EMB*)(new_emp_pos) = tmp;
-        if (emb != (EMB*)(d_par->firstFreeStart)) emb_pre->nextStart = new_emp_pos;
-        else d_par->firstFreeStart = new_emp_pos;
+        emb->size = size; emb->free = 0;
+        unsigned long new_emb_pos = (unsigned long)(emb + 1) + size;
+        *(EMB*)(new_emb_pos) = (EMB){.size = emb->size - size - sizeof(EMB), .free = 1,
+                .nextStart = emb->nextStart, .prev = emb, .prevStart = emb->prevStart};
+        if (emb->prevStart == 0) d_par->firstFreeStart = new_emb_pos;
+        else emb->prevStart->nextStart = (EMB*)new_emb_pos;
     }
-    return (unsigned long)emb;
+    return (unsigned long)(emb + 1);
 }
 
-/*
- *r
- */
 unsigned long dPartitionFreeFirstFit(unsigned long dp, unsigned long start){
 	//本函数需要实现！！！
 	/*功能：释放一个空间
-	1.按照对应的fit的算法释放空间
-	2.注意检查要释放的start~end这个范围是否在dp有效分配范围内
-		返回0 没问题
-		返回1 error
-	3.需要考虑两个空闲且相邻的内存块的合并
+        1.按照对应的fit的算法释放空间
+        2.注意检查要释放的start~end这个范围是否在dp有效分配范围内
+            返回0 没问题
+            返回1 error
+        3.需要考虑两个空闲且相邻的内存块的合并
 	*/
-	
+    unsigned long max_len = dp + ((dPartition*)dp)->size + sizeof(dPartition);
+	if (start >= max_len) return 1;
+    EMB* emb = (EMB*)start - 1;
+    if (emb->free) return 1;
+    EMB* next = (EMB*)(start + emb->size);
+    if (start + emb->size < max_len && next->free) {
+        // First remove the next block.
+        next->prevStart->nextStart = next->nextStart;
+        next->nextStart->prevStart = next->prevStart;
+        // Merge the next block into the current one.
+        emb->size += sizeof(EMB) + next->size;
+        if ((unsigned long)next + next->size + sizeof(EMB) < max_len)
+            ((EMB*)((unsigned long)next + next->size + sizeof(EMB)))->prev = emb;
+    }
+    EMB* prev = emb->prev;
+    if (prev && prev->free) {
+        prev->size += sizeof(EMB) + emb->size;
+        if (start + emb->size < max_len)
+            ((EMB*)(start + emb->size))->prev = prev;
+    }
+    else {
+        // Insert emb to the head of the list.
+        emb->nextStart = (EMB*)(((dPartition*)dp)->firstFreeStart);
+        emb->prevStart = 0;
+        emb->prev = 0;
+        emb->free = 1;
+        ((dPartition*)dp)->firstFreeStart = (unsigned long)emb;
+    }
+    return 0;
 }
 
 //wrap: we select firstFit, you can select another one
@@ -113,6 +141,6 @@ unsigned long dPartitionAlloc(unsigned long dp, unsigned long size){
 	return dPartitionAllocFirstFit(dp,size);
 }
 
-unsigned long dPartitionFree(unsigned long	 dp, unsigned long start){
+unsigned long dPartitionFree(unsigned long dp, unsigned long start){
 	return dPartitionFreeFirstFit(dp,start);
 }
