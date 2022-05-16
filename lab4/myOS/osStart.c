bb2e3e640028982257e33b69b94bb5464d3efb2a
Bill <cgx@mail.ustc.edu.cn>
2022-05-16 13:24:56 +0800

    Modify start up.

--- lab4/myOS/osStart.c
+++ lab4/myOS/osStart.c
@@ -16,9 +16,8 @@ void osStart(void)
 	enable_interrupt();
     clear_screen();
 
+#ifdef MEM_TEST
     pMemInit();  //after this, we can use kmalloc/kfree and malloc/free
-#ifndef MEM_TEST
-    clear_screen();
 #endif //MEM_TEST
 
     printk(0x2, "START RUNNING......\n");
