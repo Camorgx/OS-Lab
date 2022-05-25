0f73a250419299012b3ac81507db22bad2421d82
曹高翔 <cgx@mail.ustc.edu.cn>
2022-05-25 11:39:43 +0800

    Add FIFO Queue.

--- lab5/myOS/include/kernel/scheduling/task.h
+++ lab5/myOS/include/kernel/scheduling/task.h
@@ -4,4 +4,25 @@
 extern void CTX_SW(void);
 extern void context_switch(unsigned long **prevTskStkAddr, unsigned long *nextTskStk);
 
+typedef enum State {
+    READY,
+    WAITING
+} State;    
+
+typedef struct tskPara {
+    unsigned priority;
+    unsigned arrTime;
+    unsigned exeTime;
+} tskPara;
+
+typedef struct TCB {
+    unsigned tid; // 从 1 开始编号，0 被空 TCB 独占
+    State state;
+    unsigned long* stack;
+    tskPara params;
+} TCB;
+
+#define NULL_TCB ((TCB){.tid = 0, .state = READY, .stack = 0, \
+    .params = (tskPara){0, 0, 0}})
+
 #endif //LAB5_TASK_H
--- lab5/myOS/include/kernel/scheduling/taskQueueFIFO.h
+++ lab5/myOS/include/kernel/scheduling/taskQueueFIFO.h
@@ -1,6 +1,22 @@
 #ifndef TASKQUEUEFIFO_H
 #define TASKQUEUEFIFO_H
 
+#include "kernel/scheduling/task.h"
 
+typedef struct taskQueueFIFO {
+    TCB* data;
+    unsigned head, tail;
+    unsigned size;
+} taskQueueFIFO;
+
+extern taskQueueFIFO taskQueue;
+
+extern unsigned qpush(taskQueueFIFO* queue, TCB tcb);
+extern unsigned qpop(taskQueueFIFO* queue);
+extern TCB qfront(const taskQueueFIFO* queue);
+extern TCB qback(const taskQueueFIFO* queue);
+extern unsigned qlength(const taskQueueFIFO* queue);
+extern unsigned qfull(const taskQueueFIFO* queue);
+extern unsigned qempty(const taskQueueFIFO* queue);
 
 #endif // !TASKQUEUEFIFO_H
